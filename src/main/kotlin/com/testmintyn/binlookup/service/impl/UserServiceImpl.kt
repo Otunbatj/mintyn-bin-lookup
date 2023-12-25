dd51871aa201acc85e2de1b6324455e373669051
Ayoola Oluwatosin <tosyne.eksu@gmail.com>
2023-12-25 15:20:08 +0100

    Refactored service for Unit Test

--- src/main/kotlin/com/testmintyn/binlookup/service/impl/UserServiceImpl.kt
+++ src/main/kotlin/com/testmintyn/binlookup/service/impl/UserServiceImpl.kt
@@ -2,12 +2,14 @@ package com.testmintyn.binlookup.service.impl
 
 import com.testmintyn.binlookup.dto.UserDto
 import com.testmintyn.binlookup.exception.NotFoundException
+import com.testmintyn.binlookup.exception.UserCreationException
 import com.testmintyn.binlookup.extensions.Extensions.toUserDetails
 import com.testmintyn.binlookup.extensions.Extensions.toUserEntity
 import com.testmintyn.binlookup.model.User
 import com.testmintyn.binlookup.repository.UserRepository
 import com.testmintyn.binlookup.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -15,11 +17,20 @@ import org.springframework.stereotype.Service
 import java.util.Objects
 
 @Service
-class UserServiceImpl(private val userRepository: UserRepository, private val passwordEncoder: BCryptPasswordEncoder) : UserService, UserDetailsService {
+class UserServiceImpl(private val userRepository: UserRepository, private val passwordEncoder: BCryptPasswordEncoder) :
+    UserService, UserDetailsService {
     override fun createUser(userDto: UserDto): User {
-        //hash the user password, don't persist clear password in the storage
-        userDto.password = passwordEncoder.encode(userDto.password)
-        return userRepository.save(userDto.toUserEntity())
+        try {
+            //hash the user password, don't persist clear password in the storage
+            userDto.password = passwordEncoder.encode(userDto.password)
+            return userRepository.save(userDto.toUserEntity())
+        } catch (e: DataIntegrityViolationException) {
+            throw UserCreationException(
+                "${e.cause?.message} :: DataIntegrityViolationException"
+            )
+        } catch (e: Exception) {
+            throw UserCreationException("User creation error")
+        }
     }
 
     override fun getUserByEmail(email: String): User {
